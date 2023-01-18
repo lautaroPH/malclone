a523d721b382959ae4829e29931a2f017139b6ff
Lautaro Perez Herrera <lautaroph@gmail.com>
2023-01-18 10:33:38 -0300

    sa

--- utils/loginUrl.js
+++ utils/loginUrl.js
@@ -1,4 +1,4 @@
 import { token } from 'utils/token';
 import { v4 as uuidv4 } from 'uuid';
-
+const porbnado = 2;
 export const url = `https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=d092b0cf40e3726bd91eb1401def13b3&code_challenge=${token}&state=${uuidv4()}`;
